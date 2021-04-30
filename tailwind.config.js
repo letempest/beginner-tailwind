221577dc880b312b8d8662eee9b33582134047f2
letempest <nostalgica316@gmail.com>
2021-04-30 21:09:08 +0800

    header layout + styling + responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['src/**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        teal: colors.teal,
+        orange: colors.orange
+      }
+    }
   },
   variants: {
     extend: {}
